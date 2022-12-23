0034b532ec25b4cfb4e8c078bfab06e892368fb5
s5443626 <117823998+s5443626@users.noreply.github.com>
2022-12-23 11:00:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
-
-int main() {
-   return 0;
+float f(float y) {
+    return (((y * y) * cos(y)) + 1);
 }
+    int main() {
+        float a=0, b=0, x=0, err=0;
+        do {cout << "inserire estremi" << endl;
+            cin >> a >> b;
+        } while ((f(a)*f(b))>=0);
+
+
+        do {
+            x=((a+b)/2);
+            if (f(x)==0){
+            } else {
+                if ((f(a))*(f(x))<0){
+                    b=x;
+                }else if (((f(a))*(f(x)))>0){
+                    a=x;
+                }
+                err=(abs((b-a)/2));
+            }
+        }while (err>=0.00002);
+        x=x*10000;
+        float risultato1=int(x);
+        float risultato2=risultato1/10000;
+        cout << risultato2;
+        return 0;
+    }
